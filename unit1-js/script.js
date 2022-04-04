61e278c46b13a4efdf66dfa5edba1fe2e85d96a9
Maurice Mason <maurice.mason97@gmail.com>
2022-04-04 14:50:21 -0400

    Arrays

---
+++ unit1-js/script.js
@@ -0,0 +1,22 @@
+document.write("Javascript works!");
+console.log("Hello Console!");
+console.warn("This is only a test!");
+let myName = "Maurice Mason";
+let workFromHome = true;
+let side = 15;
+let radius = 10;
+console.log(`My name is ${myName}.`);
+console.log(`I work from home: ${workFromHome}`);
+console.log("The length of each side of the square is " + side);
+console.log("The radius of the circle is " + radius);
+let squareArea = side * side;
+let squarePerimeter = side * 4;
+let cirlceArea = Math.PI * radius * radius;
+let circlePerimeter = 2 * Math.PI * radius;
+console.log("The square area is " + squareArea + " and the perimeter is " + squarePerimeter);
+console.log("The cirlce area is " + cirlceArea + " and the perimeter is " + circlePerimeter);
+let travelOptions = ["foot", "bike", "car", "airplane"]
+console.log(`1) ${travelOptions[0]}`);
+console.log(`2) ${travelOptions[1]}`);
+console.log(`3) ${travelOptions[2]}`);
+console.log(`4) ${travelOptions[3]}`);
